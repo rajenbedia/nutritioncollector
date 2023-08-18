4058441997c7664d860002fb1cb9a1f2ac43afdf
rajenbedia <rajenbedia@gmail.com>
2023-08-18 11:35:45 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <HomePage />
+</template>
+
+<script>
+import HomePage from "@/components/HomePage.vue";
+export default {
+  name: "App",
+  components: {
+    HomePage,
+  },
+};
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>HOME PAGE</h1>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
