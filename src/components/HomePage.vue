ea7b964d7c131f506a2eaebc8768e34a60e46ea3
rajenbedia <rajenbedia@gmail.com>
2023-08-18 15:47:31 +0100

    Scanner changes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,56 @@
 <template>
   <h1>HOME PAGE</h1>
+  <button @click="showBarcodeScanner">Scan</button>
+  <StreamBarcodeReader
+    v-if="showScanner"
+    @decode="searchFood"
+  ></StreamBarcodeReader>
+  {{ test }}
 </template>
 
 <script>
+import axios from "axios";
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
 export default {
   name: "HomePage",
+  components: {
+    StreamBarcodeReader,
+  },
+  data() {
+    return {
+      showScanner: false,
+      test: "test",
+    };
+  },
+  methods: {
+    showBarcodeScanner() {
+      this.showScanner = true;
+    },
+    async searchFood(barcodeNumber) {
+      const headers = {
+        "User-Agent":
+          "nutritioncollector" -
+          "PC" -
+          "Version 1.0" -
+          "http://localhost:5173/",
+      };
+
+      const url = `https://world.openfoodfacts.org/api/v2/product/${barcodeNumber}`;
+
+      const response = await axios.get(url, { headers });
+      const data = response.data.product;
+      console.log("Carbs:" + data.nutriments.carbohydrates);
+      console.log("Fats:" + data.nutriments.fat);
+      console.log("Fibre:" + data.nutriments.fiber);
+      console.log("Proteins:" + data.nutriments.proteins);
+      console.log("Kcals: " + data.nutriments["energy-kcal"]);
+
+      console.log("Total Quantity:" + data.quantity);
+
+      this.test = "Carbs:" + data.nutriments.carbohydrates;
+      this.showScanner = false;
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "@/index.css";
+
+const YOUR_APP_ID = "095c7096"; // Your APP ID
+const YOUR_API_KEY = "3c6fe6fd7fc7a35125a69d4677bef903"; // Your KEY
+
 createApp(App).mount("#app");
